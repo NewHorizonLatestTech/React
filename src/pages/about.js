ae3f5b66cf1b7786ca821e747a137c677bfbda54
RanaRabees <adeemaamir1722@gmail.com>
2023-04-06 17:22:52 +0500

    React
    React

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../styles/Home.module.css'
+import Link from 'next/link'
+function index() {
+  return (
+    <>
+   <div className='aboutrocket'>
+   <h1 className='textrocket'>About Rocket</h1>
+      <button className='about'>
+        <Link href="/">
+          Play Again
+        </Link>
+      </button>
+   </div>
+      <main className='display'>
+        <h2 className='textsub'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+          Sapiente, eos esse voluptatum assumenda architecto praesentium sequi <br />
+          dicta molestias modi eligendi tempore delectus? <br />
+          Perferendis libero laboriosam fugiat laudantium, <br />
+          perspiciatis sequi quasi?<br />
+        </h2>
+
+        <iframe width="844" height="4149" src="https://rive.app/s/YiNefvUdkEKME1IcHRYNGA/embed" allowfullscreen></iframe>
+
+      </main>
+    </>
+  )
+}
+
+export default index;
